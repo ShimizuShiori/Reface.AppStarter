d2df9d36b126857fc52b3e646e60fd3131e02c57
ShimizuShiori <116971263@qq.com>
2020-03-23 11:53:19 +0800

    向 nuspec 文件添加对 CommandBus 的添加，新增 CommandHandlerAttribute

---
+++ src/Reface.App.Starter/Attributes/CommandHandlerAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reface.AppStarter.Attributes
+{
+    /// <summary>
+    /// 命令总线模式下的命令接受者的特征类
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class CommandHandlerAttribute : ComponentAttribute
+    {
+    }
+}
