70afdb1cd777dba4108ac6dbb6a7cea7d1bf3e7f
ShimizuShiori <116971263@qq.com>
2020-07-13 18:53:22 +0800

    添加新的扩展方法

---
+++ src/Reface.App.Starter/Extensions/AppExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Reface.AppStarter.AppContainers;
+
+namespace Reface.AppStarter
+{
+    public static class AppExtensions
+    {
+        /// <summary>
+        /// 创建组件
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static T CreateComponent<T>(this App app)
+        {
+            IComponentContainer container = app.GetAppContainer<IComponentContainer>();
+            return container.CreateComponent<T>();
+        }
+    }
+}
